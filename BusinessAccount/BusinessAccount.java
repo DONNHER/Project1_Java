dc5551637f684f3b309aaa11be4da0adda0acf56
seriouslymatcha <roshanjacobe@gmail.com>
2025-03-13 23:33:17 +0800

    Update BusinessAccount.java

--- BusinessAccount/BusinessAccount.java
+++ BusinessAccount/BusinessAccount.java
@@ -6,114 +6,125 @@ import Bank.Bank;
 import CreditAccount.CreditAccount;
 import SavingsAccount.SavingsAccount;
 
+/*
+  A BusinessAccount that supports deposits, withdrawals, transfers, and payments.
+ */
 public class BusinessAccount extends Account implements Deposit, Withdrawal, FundTransfer, Payment {
     private double balance = 0;
     private final String company;
-    public BusinessAccount(Bank bank,String company, String accountNumber, String firstName ,String lastName, String email, String pin,double balance) {
+
+    /*
+     Creates a BusinessAccount with the given details.
+     */
+    public BusinessAccount(Bank bank, String company, String accountNumber, String firstName, String lastName, String email, String pin, double balance) {
         super(bank, accountNumber, firstName, lastName, email, pin);
         this.balance = balance;
         this.company = company;
     }
-    public String getCompany(){
+
+    /*
+     Gets the name of the company linked to this account.
+     @return The name of the company.
+     */
+    public String getCompany() {
         return this.company;
     }
-    public double getBalance(){
+
+    /*
+     Gets the current balance of the account.
+     @return The account balance.
+     */
+    public double getBalance() {
         return this.balance;
     }
+
     /*
-    Get the account balance statement of this savings account.
-Returns:
-String balance statement.
+     Shows the account balance statement.
      */
     @Override
     public void getAccountBalanceStatement() {
-        //Complete this method
-        String s = "Account Number: "+getAccountNumber()+"\n";
-        s += "Company: " +getCompany() +"\n";
-        s += "Name: " +getOwnerFullName() +"\n";
+        String s = "Account Number: " + getAccountNumber() + "\n";
+        s += "Company: " + getCompany() + "\n";
+        s += "Name: " + getOwnerFullName() + "\n";
         s += "Account Balance: " + this.balance;
         System.out.println(s);
     }
+
     /*
-    Validates whether this savings account has enough balance to proceed with such a transaction based on the amount that is to be adjusted.
-    @Params amount – Amount of money to be supposedly adjusted from this account’s balance.
-    @Returns Flag if transaction can proceed by adjusting the account balance by the amount to be changed.
+     Checks if there is enough balance for a transaction.
+     @param amount The amount to check against the current balance.
+     @return True if there is enough balance, false otherwise.
      */
-    private boolean hasEnoughBalance(double amount){
-        //Complete this method
+    private boolean hasEnoughBalance(double amount) {
         return this.balance >= amount;
     }
+
     /*
-    Warns the account owner that their balance is not enough for the transaction to proceed
-successfully.
+     Displays a message when there is not enough balance.
      */
     private void insufficientBalance() {
-        //Complete this method
         System.out.println("Insufficient balance for this transaction.");
-
     }
+
     /*
-    Adjust the owner’s current loan. Result of adjustment cannot be less than 0.
-    @Param amountAdjustment – Amount to be adjusted to the loan of this credit account.
+     Adjusts the account balance by a specified amount.
+     @param amountAdjustment The amount to be added or subtracted from the balance.
      */
-    private synchronized void adjustAccountBalance(double amountAdjustment){
-        //Complete this method
+    private synchronized void adjustAccountBalance(double amountAdjustment) {
         this.balance += amountAdjustment;
-        if (balance < 0){
+        if (balance < 0) {
             this.balance = 0.0;
         }
     }
 
+    /*
+     Deposits money into the account if it's below the deposit limit.
+     @param amount The amount of money to be deposited.
+     @return True if the deposit was successful, false otherwise.
+     */
     @Override
     public synchronized boolean cashDeposit(double amount) {
-        //Complete this method
         if (amount < getBank().getDepositLimit()) {
             adjustAccountBalance(amount);
             return true;
         }
         return false;
     }
+
     /*
-        Transfers an amount of money from this account to another savings account. Is extensively used
-    by the other transfer() method.
-    Params:
-    account – Account number of recipient.
-    amount – Amount of money to be supposedly adjusted from this account’s balance.
-    Returns:
-    Flag if fund transfer transaction is successful or not.
-    Throws:
-    IllegalAccountType – Cannot fund transfer when the other account is of type
-    CreditAccount.
-         */
+     Transfers money to another account within the same bank.
+     @param account The recipient account.
+     @param amount The amount of money to transfer.
+     @return True if the transfer was successful, false otherwise.
+     @throws IllegalAccountType If the recipient account is a CreditAccount.
+     */
     @Override
     public synchronized boolean transfer(Account account, double amount) throws IllegalAccountType {
-        //Complete this method
         if (account instanceof CreditAccount) {
-            throw new IllegalAccountType("Cannot transfer funds to a non-savings account.");
+            throw new IllegalAccountType("Cannot transfer funds to a CreditAccount.");
         }
         if (hasEnoughBalance(amount)) {
             adjustAccountBalance(-amount);
-            ((BusinessAccount) account ).adjustAccountBalance(amount);
+            ((BusinessAccount) account).adjustAccountBalance(amount);
             return true;
         } else {
             insufficientBalance();
             return false;
         }
     }
-    /*
-    Transfers an amount of money from this account to another savings account. Should be used when transferring to other banks.
-    @Params bank – Bank object of the recipient.
-    @Params account – Account number of recipient.
-    @Params amount – Amount of money to be supposedly adjusted from this account’s balance.
-    @Returns Flag if fund transfer transaction is successful or not.
-    @Throws IllegalAccountType – Cannot fund transfer when the other account is of type CreditAccount.
-     */
 
+    /*
+     Transfers money to an account in a different bank.
+     @param bank The recipient's bank.
+     @param account The recipient account.
+     @param amount The amount of money to transfer.
+     @return True if the transfer was successful, false otherwise.
+     @throws IllegalAccountType If the recipient account is a CreditAccount.
+     */
     @Override
-    public boolean transfer(Bank bank,Account account, double amount) throws IllegalAccountType {
-        //Complete this method
+    public boolean transfer(Bank bank, Account account, double amount) throws IllegalAccountType {
         if (account instanceof CreditAccount) {
-            throw new IllegalAccountType("Cannot fund transfer when the other account is of type CreditAccount.");
+            throw new IllegalAccountType("Cannot transfer to a CreditAccount.");
         }
         Account toTransfer = getBank().getBankAccount(bank, account.getAccountNumber());
         if (hasEnoughBalance(amount)) {
@@ -126,37 +137,49 @@ successfully.
         }
     }
 
+    /*
+     Withdraws money from the account if there is enough balance and it's within the bank's limit.
+     @param amount The amount of money to withdraw.
+     @return True if the withdrawal was successful, false otherwise.
+     */
     @Override
     public synchronized boolean withdrawal(double amount) {
         if (hasEnoughBalance(amount)) {
-            if(amount <= this.getBank().getWithdrawLimit()) {
+            if (amount <= this.getBank().getWithdrawLimit()) {
                 adjustAccountBalance(-amount);
                 return true;
             }
-            System.out.println("Withdraw Failed: The amount to be withdraw exceeds to Bank withdrawal limit.");
+            System.out.println("Withdraw Failed: The amount exceeds the bank's withdrawal limit.");
             return false;
         }
         insufficientBalance();
         return false;
     }
 
-
-    public String toString(){
-        return "Name: " + this.getOwnerFullName();
-    }
-
+    /*
+     Makes a payment to another account if there is enough balance.
+     @param account The recipient account to be paid.
+     @param amount The amount to pay.
+     @return True if the payment was successful, false otherwise.
+     @throws IllegalAccountType If the recipient account is a CreditAccount.
+     */
     @Override
     public synchronized boolean pay(Account account, double amount) throws IllegalAccountType {
         if (account instanceof CreditAccount) {
-            // Throw an exception if it's a CreditAccount
-            throw new IllegalAccountType("Credit Accounts cannot pay to other Credit Accounts as they do not hold account money balance.");
+            throw new IllegalAccountType("Cannot pay to a CreditAccount.");
         }
         if (hasEnoughBalance(amount)) {
-            // If it's a valid SavingsAccount (or other type), perform the payment
-            // Assuming there's a method to deposit the money in the account
             SavingsAccount payed = (SavingsAccount) account;
             return payed.cashDeposit(amount);
         }
         return false;
+    }
+
+    /*
+     Returns the owner's full name as a string.
+     @return The owner's full name.
+     */
+    public String toString() {
+        return "Name: " + this.getOwnerFullName();
     }
 }
